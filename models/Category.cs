f00ce5d399ffe201d8e3c195a4973944bb5ff7ba
AlexFerreira10 <correaferreiraalex92@gmail.com>
2025-03-29 10:56:12 -0300

    feat: relationship between entities + data annotations

--- models/Category.cs
+++ models/Category.cs
@@ -1,7 +1,26 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CatalogApi.Models;
 
+[Table("Categories")]
 public class Category {
+
+    public Category() {
+        Products = new Collection<Product>();
+    }
+
+    [Key]
     public int CategoryId { get; set;}
+    
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
-    public string? ImageUrl { get; set;}
+
+    [Required]
+    [StringLength(300)]
+    public string? ImageUrl { get; set; }
+
+    public ICollection<Product>? Products {get; set;}
 }
