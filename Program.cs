28e87d1b98b995056ff0958d5ba83aeb635f5f61
AlexFerreira10 <correaferreiraalex92@gmail.com>
2025-03-29 10:53:45 -0300

    feat: ef core configurations

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using CatalogApi.Context;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection))
+);
 
 var app = builder.Build();
 
